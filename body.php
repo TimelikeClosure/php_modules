ce639a1c6f4f3bba6624e07ff730080f703ae261
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-08-19 13:10:46 -0700

    Added Module::get cacheing test

--- body.php
+++ body.php
@@ -1,3 +1,8 @@
 <?php
 $moduleScopedVariable = FALSE;
+if (!isset($runCount)){
+    $runCount = 0;
+}
+$runCount++;
 $exports = '<h1>Welcome to the World of Modules!</h1>';
+$exports .= "<p>This file has run $runCount times</p>";
--- index.php
+++ index.php
@@ -10,9 +10,13 @@ require_once(__DIR__.'/dependencies.php');
     $body = Module::get(__DIR__.'/body.php');
     if (isset($moduleScopedVariable)){
       echo '<h1>ERROR: Module scope isolation test failed!</h1>';
+      exit();
     } else {
       echo $body;
     }
+    echo Module::get(__DIR__.'/body.php');
+    echo Module::get(__DIR__.'/body.php');
+    echo Module::get(__DIR__.'/body.php');
     ?> 
   </body>
 </html>
