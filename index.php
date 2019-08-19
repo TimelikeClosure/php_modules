7bdb6490a5ca814d4efc9d512c979a55319d8bd4
Tim Horist <TimelikeClosure@users.noreply.github.com>
2019-08-19 13:17:12 -0700

    Reorganized file structure to be more library-ish

--- index.php
+++ index.php
@@ -1,22 +1,2 @@
 <?php
-require_once(__DIR__.'/dependencies.php');
-?>
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php
-    $body = Module::get(__DIR__.'/body.php');
-    if (isset($moduleScopedVariable)){
-      echo '<h1>ERROR: Module scope isolation test failed!</h1>';
-      exit();
-    } else {
-      echo $body;
-    }
-    echo Module::get(__DIR__.'/body.php');
-    echo Module::get(__DIR__.'/body.php');
-    echo Module::get(__DIR__.'/body.php');
-    ?> 
-  </body>
-</html>
+require_once(__DIR__.'/lib/module.php');
